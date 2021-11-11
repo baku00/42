baddf055d93d07e81e1f1f3e3eead710099f8582
David Gloriod <dgloriod@c2r2s4.42lausanne.ch>
2021-11-11 18:44:47 +0100

    Save

--- get_next_line/get_next_line/get_next_line.c
+++ get_next_line/get_next_line/get_next_line.c
@@ -3,46 +3,33 @@
 
 char  *get_next_line(int fd)
 {
-  int   i;
-  int   ret;
-  char  buf[2];
-  char  *temp;
-  char  *before_return;
-  char  *result;
+	int   i;
+	int   ret;
+	char  buf[2];
+	char  *temp;
+	char  *result;
 
-  buf[1] = '\0';
-  result = NULL;
-  temp = NULL;
-  i = 0;
-  while ((ret = read(fd, buf, 1))) {
-    if (ret == -1)
-    {
-      if (result)
-        free(result);
-      return (0);
-    }
-    if (!result)
-      result = "";
-    temp = ft_strjoin(result, buf);
-    result = ft_strdup(temp);
-    free(temp);
-    i++;
-    if (buf[0] == '\n')
-      break;
-  }
-  printf("Malloc_size: (%zu) ", malloc_size(result));
-  if (result)
-  {
-    before_return = ft_strdup(result);
-    free(result);
-    printf("Malloc_size: (%zu) ", malloc_size(before_return));
-    printf("=> (%zu)", ft_strlen(before_return));
-  }
-  printf("\n");
-  if (i == 0)
-  {
-    printf("Malloc_size 1: (%zu)\n", malloc_size(before_return));
-    return (0);
-  }
-  return (before_return);
+	buf[1] = '\0';
+	result = NULL;
+	temp = NULL;
+	i = 0;
+	while ((ret = read(fd, buf, 1))) {
+		if (ret == -1)
+		{
+			if (result)
+				free(result);
+			return (0);
+		}
+		if (!result)
+			result = "";
+		temp = ft_strjoin(result, buf);
+		result = ft_strdup(temp);
+		free(temp);
+		i++;
+		if (buf[0] == '\n')
+			break;
+	}
+	if (i == 0)
+		return (0);
+	return (result);
 }
