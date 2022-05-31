f7f17136ff998b3020516d322be87d8ce72b2661
David Gloriod <dgloriod@c1r14s4.42lausanne.ch>
2022-05-31 19:05:21 +0200

    Finish

--- so_long/basic/so_long.c
+++ so_long/basic/so_long.c
@@ -6,7 +6,7 @@
 /*   By: dgloriod <dgloriod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 18:33:18 by dgloriod          #+#    #+#             */
-/*   Updated: 2022/04/19 21:26:17 by dgloriod         ###   ########.fr       */
+/*   Updated: 2022/05/31 18:58:36 by dgloriod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int	main(int argc, char **argv)
 	mlx_key_hook(mlx.window.ptr, key_hook, &mlx);
 	mlx_hook(mlx.window.ptr, 17, 1L << 17, mlx_close, &mlx);
 	printf("\n\n\nDébut du jeu\n");
+	print_ath(&mlx);
 	mlx_loop(mlx.ptr);
 	return (0);
 }
--- so_long/bonus/srcs/functions/move_bonus.c
+++ so_long/bonus/srcs/functions/move_bonus.c
@@ -6,12 +6,43 @@
 /*   By: dgloriod <dgloriod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 18:34:31 by dgloriod          #+#    #+#             */
-/*   Updated: 2022/05/28 02:53:46 by dgloriod         ###   ########.fr       */
+/*   Updated: 2022/05/31 18:54:31 by dgloriod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../basic/so_long.h"
 #ifdef WITH_BONUS
+
+static void	register_image(t_mlx *mlx, void *img, int x, int y)
+{
+	if (img)
+		mlx_put_image_to_window(mlx->ptr, mlx->window.ptr, \
+			img, x * ITEM_WIDTH, y * ITEM_HEIGHT);
+	else
+		ft_exit(mlx, "Erreur lors du placement d'une image", 1);
+}
+
+static void	animations_bonus(t_mlx *mlx)
+{
+	size_t	x;
+	size_t	y;
+	void	*img;
+
+	if (mlx->elements.player.movement % 2)
+		img = mlx->elements.ia.img;
+	else
+		img = mlx->elements.ia.img_2;
+	y = -1;
+	while (++y < mlx->file.height)
+	{
+		x = -1;
+		while (++x < mlx->file.width - 1)
+		{
+			if (mlx->file.point[y][x] == IA)
+				register_image(mlx, img, x, y);
+		}
+	}
+}
 
 static int	move_bonus(t_mlx *mlx, int y, int x)
 {
@@ -28,6 +59,7 @@ static int	move_bonus(t_mlx *mlx, int y, int x)
 
 int	can_move(t_mlx *mlx, int y, int x)
 {
+	animations_bonus(mlx);
 	return (move_bonus(mlx, y, x));
 }
 #endif
