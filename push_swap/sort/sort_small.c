baeb10e0a9cf6ea318c152d5ee7885f55dd76cf1
baku2426@gmail.com <baku2426@gmail.com>
2022-11-07 00:59:53 +0100

    Before test

--- push_swap/sort/sort_small.c
+++ push_swap/sort/sort_small.c
@@ -6,7 +6,7 @@
 /*   By: dgloriod <dgloriod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 04:08:55 by dgloriod          #+#    #+#             */
-/*   Updated: 2022/11/06 17:35:52 by dgloriod         ###   ########.fr       */
+/*   Updated: 2022/11/07 00:59:26 by dgloriod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,21 @@ static t_sort	sort_three(int *a, int a_counter)
 	t_sort	sort;
 
 	if (a[0] > a[1] && a[2] && a[2] > a[0])
-		(ft_printf("sa\n"), a = sa(a));
+		a = s(a, 'a', 1);
 	else if (a[0] > a[1] && a[1] > a[2] && a[2] < a[0])
 	{
-		(ft_printf("sa\n"), a = sa(a));
+		a = s(a, 'a', 1);
 		a = rr(a, a_counter, 'a', 1);
 	}
 	else if (a[0] > a[1] && a[1] < a[2] && a[2] < a[0])
 		a = r(a, a_counter, 'a', 1);
 	else if (a[0] < a[1] && a[1] > a[2] && a[2] > a[0])
 	{
-		(ft_printf("sa\n"), a = sa(a));
+		a = s(a, 'a', 1);
 		a = r(a, a_counter, 'a', 1);
 	}
 	else if (a[0] < a[1] && a[1] > a[2] && a[2] < a[0])
-	{
 		a = rr(a, a_counter, 'a', 1);
-	}
 	sort.a = a;
 	return (sort);
 }
@@ -54,18 +52,12 @@ static int	find_min(int *stack, int counter)
 	return (min);
 }
 
-static t_sort	sort_four(int *a, int *b, int a_counter, int b_counter)
+static t_sort	sort_four(t_sort sort)
 {
 	int		bingo;
 	int		min;
-	t_sort	sort;
-	t_sort	reverse;
 
 	bingo = 0;
-	sort.a = a;
-	sort.b = b;
-	sort.a_counter = a_counter;
-	sort.b_counter = b_counter;
 	min = find_min(sort.a, sort.a_counter);
 	while (!bingo)
 	{
@@ -76,33 +68,25 @@ static t_sort	sort_four(int *a, int *b, int a_counter, int b_counter)
 		}
 		else if (sort.a[1] == min)
 		{
-			sort.a = sa(sort.a);
-			ft_printf("sa\n");
+			sort.a = s(sort.a, 'a', 1);
 			sort = pb(sort);
 			bingo++;
 		}
 		else
-		{
 			sort.a = r(sort.a, sort.a_counter, 'a', 1);
-		}
 	}
 	sort_three(sort.a, sort.a_counter);
 	sort = pa(sort);
 	return (sort);
 }
 
-static void	sort_five(int *a, int *b, int a_counter, int b_counter)
+static void	sort_five(t_sort sort)
 {
-	t_sort	sort;
 	t_sort	reverse;
 	int		bingo;
 	int		min;
 
 	bingo = 0;
-	sort.a = a;
-	sort.b = b;
-	sort.a_counter = a_counter;
-	sort.b_counter = b_counter;
 	while (bingo < 2)
 	{
 		min = find_min(sort.a, sort.a_counter);
@@ -113,8 +97,7 @@ static void	sort_five(int *a, int *b, int a_counter, int b_counter)
 		}
 		else if (a[1] == min)
 		{
-			sort.a = sa(a);
-			ft_printf("sa\n");
+			sort.a = s(sort.a, 'a', 1);
 			sort = pb(sort);
 			bingo++;
 		}
@@ -128,14 +111,8 @@ static void	sort_five(int *a, int *b, int a_counter, int b_counter)
 
 void	sort_small(t_sort sort)
 {
-	if (sort.a_counter == 2)
-	{
-		if (sort.a[0] > sort.a[1])
-		{
-			sa(sort.a);
-			ft_printf("sa\n");
-		}
-	}
+	if (sort.a_counter == 2 && sort.a[0] > sort.a[1])
+		sort.a = s(sort.a, 'a', 1);
 	else if (sort.a_counter == 3)
 		sort_three(sort.a, sort.a_counter);
 	else if (sort.a_counter == 4)
